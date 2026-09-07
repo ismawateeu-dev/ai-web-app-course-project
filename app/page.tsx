f53cbed32290c21ce3baf403c95fca60541c8b41
ismawateeu-dev <ismawatee.u@rmutsvmail.com>
2026-09-07 12:55:08 +0700

    detection

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
-import { AppHeader } from "./components/AppHeader";
-import { DetectionPanel } from "./components/DetectionPanel";
-import { FeatureCard } from "./components/FeatureCard";
+import { AppHeader } from "@/components/AppHeader";
+import { FeatureCard } from "@/components/FeatureCard";
+import { DetectionPanel } from "@/components/DetectionPanel";
+import { ApiStatus } from "@/components/ApiStatus";
 export default function Home() {
   return (
     <main>
       <AppHeader />
+      <br></br>
       <FeatureCard
         title="Object Detection"
         description="ตรวจจับวัตถุจากรูปภาพด้วย AI"
       />
+      <br></br>
       <FeatureCard
         title="AI Chat"
         description="สนทนากับ Generative AI"
       />
-      
-      < DetectionPanel/>
-      
+      <br></br>
+      <DetectionPanel />
+      <br></br>
+      <ApiStatus />
     </main>
   );
 }
